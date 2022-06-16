eb031ba5047239cf5cfaa0593517e26277058869
Puneet Verma <puneetv178@gmail,com>
2022-06-16 17:04:14 +0530

    #New Changes

---
+++ B.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public abstract class B: Interface
+    {
+        Security t = new Security();
+        public void A1()
+        {
+
+        }
+    }
+}
